f7a343e51f6e9927860e09c5bfe1242051eacf0b
youngeun <summer_97@naver.com>
2021-04-03 13:55:04 +0900

    fix scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Grid,Text,Button} from '../elements';
 import HeaderLeftImg  from '../images/header-left-delivery.gif';
 
@@ -9,8 +10,12 @@ const Header = (props) => {
     <React.Fragment>
       <Grid bg="beige">
         <Grid flex width="1050px" margin="0px auto" bg="white" height="37px">
-          <img style={{cursor:'pointer'}}src={HeaderLeftImg} width="163px"/>
-         
+          <img style={{cursor:'pointer', margin:'3px 0px 0px 0px'}}src={HeaderLeftImg} width="163px"/>
+          <HeaderMenu>
+            <li>회원가입</li>
+            <li>로그인</li>
+            <li>고객센터</li>
+          </HeaderMenu>
         </Grid>
          
       </Grid>
@@ -20,3 +25,17 @@ const Header = (props) => {
 };
 
 export default Header;
+
+const HeaderMenu = styled.ul`
+  list-style-type: none;
+  display: flex;
+  font-size: 12px;
+ 
+  & li{
+    padding: 0px 22px 0px 0px;
+
+  }
+  & li:nth-child(1){
+    color:#5f0080;
+  }
+`;
--- src/shared/App.js
+++ src/shared/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configStore";
 import { Header } from "../components";
+import '../scss/main.scss';
 function App() {
   return (
     <React.Fragment>
