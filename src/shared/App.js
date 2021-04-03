20052feee81f0fe5364e1e80d48b8bebcd828000
youngeun <summer_97@naver.com>
2021-04-03 10:23:05 +0900

    project setting folder

---
+++ src/shared/App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configStore"
+
+function App() {
+  return (
+    <React.Fragment>
+      <h2>마켓컬리</h2>
+   </React.Fragment>
+  );
+}
+
+export default App;
